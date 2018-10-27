41ee6bf2d9369a4220ad17ba0a9e76cd1f818291
rayedchan <rayedchan@gmail.com>
2018-10-27 09:25:08 -0400

    use env variables

--- icims.py
+++ icims.py
@@ -8,7 +8,7 @@ print(API_KEY)
 
 #curl -X GET https://hackicims.com/api/v1/companies/32/jobs -H 'Authorization: Bearer <API_KEY>' -H 'Content-Type: application/json' | jq .
 # GET Request to get company information
-headers = {"Authorization": "Bearer e0add267c28fb96c6ff77194434d446d4dd3faadc996be6f9e2af00a5f425bb59a4609ea62e1040c0b42228dfd4cf695ecb56f771bfb43d44f67819fc5155473","Content-Type":"application/json"}
+headers = {"Authorization": "Bearer " + API_KEY,"Content-Type":"application/json"}
 r = requests.get("https://hackicims.com/api/v1/companies/32", headers=headers)
 print(r.json())
 # Do something with r.json() or r.text
